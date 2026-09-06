f3323103225f41839edcfe47d260b2088f65ea42
Franciszek Foltynski <birbuhs@gmail.com>
2026-09-06 07:19:30 +0200

    format main(). Add fullscreen mode.

--- src/main.rs
+++ src/main.rs
@@ -3,32 +3,35 @@ mod map;
 mod products;
 mod ships;
 
-use bevy::{math::DVec2, prelude::*};
+use bevy::{math::DVec2, prelude::*, window::WindowMode::BorderlessFullscreen};
 
 use crate::{
-    map::{EntityPosInASector, Factions, IconType, MapCamera, MapIcon, MapPlugin, MapRoute, MapState, MapVisible, Order, draw_sector, get_visible_map_objects, render_map_icons, render_routes, spawn_sector}, ships::{Fighter, Ship, ShipType},
+    map::{
+        EntityPosInASector, Factions, IconType, MapCamera, MapIcon, MapPlugin, MapRoute, MapState,
+        MapVisible, Order, draw_sector, get_visible_map_objects, render_map_icons, render_routes,
+        spawn_sector,
+    },
+    ships::{Fighter, Ship, ShipType},
 };
 
-pub fn test_map(mut commands: Commands/*, mut next_state: ResMut<NextState<MapState>>*/) {
+pub fn test_map(mut commands: Commands /*, mut next_state: ResMut<NextState<MapState>>*/) {
     let owner = Factions::Petakians;
     let pos = DVec2::new(13., 17.);
-    let icon_type = IconType::Ship(ShipType::Fighter(Fighter::new(
-                1,
-                pos.clone(),
-                owner,
-            )));
-    commands.spawn((
-        icon_type.clone(),
-        owner,
-        EntityPosInASector { sector: 1, pos },
-        MapVisible,
-    )).with_children(|parent| {
-        parent.spawn((
-            MapIcon::new(icon_type.clone(), map::RelationType::Friendly),
-            MapRoute::new(pos.clone(), DVec2::new(1000., 100.)),
-            Order::Fly,
-        ));
-    });
+    let icon_type = IconType::Ship(ShipType::Fighter(Fighter::new(1, pos.clone(), owner)));
+    commands
+        .spawn((
+            icon_type.clone(),
+            owner,
+            EntityPosInASector { sector: 1, pos },
+            MapVisible,
+        ))
+        .with_children(|parent| {
+            parent.spawn((
+                MapIcon::new(icon_type.clone(), map::RelationType::Friendly),
+                MapRoute::new(pos.clone(), DVec2::new(1000., 100.)),
+                Order::Fly,
+            ));
+        });
 
     commands.spawn(Camera2d);
     // next_state.set(MapState::Sector(1));
@@ -37,13 +40,33 @@ pub fn test_map(mut commands: Commands/*, mut next_state: ResMut<NextState<MapSt
 fn main() {
     let mut app = App::new();
     app.insert_resource(MapCamera {
-            center: DVec2::new(0., 0.),
-            zoom: 2.,
-            viewport_size: Vec2::new(160., 90.)
-        })
-        .add_systems(Startup, (test_map, spawn_sector, get_visible_map_objects, render_map_icons).chain())
-        .add_systems(Update, (render_routes, draw_sector))
-        .add_plugins((DefaultPlugins, MapPlugin))
-        .insert_state(MapState::Sector(1))
-        .run();
+        center: DVec2::new(0., 0.),
+        zoom: 2.,
+        viewport_size: Vec2::new(160., 90.),
+    })
+    .add_systems(
+        Startup,
+        (
+            test_map,
+            spawn_sector,
+            get_visible_map_objects,
+            render_map_icons,
+        )
+            .chain(),
+    )
+    .add_systems(Update, (render_routes, draw_sector))
+    .add_plugins((
+        DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                mode: BorderlessFullscreen(MonitorSelection::Primary),
+                title: "-4Y: Frustration. A game inspired by X series.".to_string(),
+                ..default()
+            }),
+            close_when_requested: true,
+            ..default()
+        }),
+        MapPlugin,
+    ))
+    .insert_state(MapState::Sector(1))
+    .run();
 }
